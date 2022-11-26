cd1d04065ab2403bce845c3edb625f82e94cecf5
Medet Gaidarov <mgaiidarov@gmail.com>
2022-11-26 16:53:36 +0500

    ignoring swagger was added

--- src/main/java/com/example/serverinstagram/configuration/security/SecurityConfiguration.java
+++ src/main/java/com/example/serverinstagram/configuration/security/SecurityConfiguration.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -23,6 +24,17 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     private final CustomAuthenticationProvider customAuthenticationProvider;
     private final UserDetailsService userDetailsService;
 
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/v2/api-docs",
+                "/configuration/ui",
+                "/swagger-resources/**",
+                "/configuration/security",
+                "/swagger-ui.html",
+                "/webjars/**",
+                "h2-console/**"
+        );
+    }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
